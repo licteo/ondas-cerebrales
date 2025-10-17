a74dcfd512899c343583174732e96e8632bd14aa
licteo <134066234+licteo@users.noreply.github.com>
2025-10-17 06:07:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const app = document.getElementById('app');
+  app.innerHTML = `<p>Haz clic en los días de la semana para ver tus actividades espirituales.</p>`;
+});
